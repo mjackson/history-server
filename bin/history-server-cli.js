a12030c4688cdc475d1f3a6405383ce3f446bb5c
Michael Jackson <mjijackson@gmail.com>
2016-11-15 11:50:53 -0800

    Added -a flag for easily serving many apps
    Fixes #1

--- bin/history-server-cli.js
+++ bin/history-server-cli.js
@@ -1,80 +1,131 @@
 #!/usr/bin/env node
-;(function () {
-  const path = require('path')
-  const { createServer } = require('../modules/index')
+const path = require('path')
+const { createServer, createAppsConfig } = require('../modules')
 
-  const argv = process.argv
+const argv = process.argv
 
-  const hasFlag = (flag) =>
-    argv.indexOf(flag) !== -1
+const hasFlag = (flag) =>
+  argv.indexOf(flag) !== -1
 
-  const getArg = (flag, defaultValue) => {
-    const index = argv.indexOf(flag)
-    return index === -1 ? defaultValue : argv[index + 1]
-  }
+const getArg = (flag, defaultValue) => {
+  const index = argv.indexOf(flag)
+  return index === -1 ? defaultValue : argv[index + 1]
+}
 
-  const showUsage = () => {
-console.log(`
+const printUsage = () => {
+  console.log(`
   ## Usage
 
+    history-server -a apps
     history-server -c config.js
     history-server root
 
   ## Flags
 
+    -a, --apps      The path to a directory that contains many apps
     -c, --config    The path to a module that exports the server config
     -h, --help      Show this help message
 
   ## Configuration
 
-  The history-server configuration should be an array of
-  { path, root, options } objects where:
+  To serve a single app, just point history-server at a root directory
+  that contains an index.html file to serve at the / URL.
 
-    - path is the URL pattern, i.e. /the/base/url*
+  To serve many apps, you'll need a way to tell history-server which apps
+  should be served at which URLs. The simplest way to do this is to just
+  use the file system to layout your apps like you want your URLs to look.
+
+  For example, consider the following directory tree:
+
+  apps/
+  ├── one
+  │   ├── index.html
+  │   └── index.js
+  └── two
+      ├── index.html
+      ├── index.js
+      └── three
+          ├── index.html
+          └── index.js
+
+  You can use "history-server -a apps" serve all 3 of these apps at the
+  following URLs, in matching order:
+
+    /two/three => apps/two/three
+    /one       => apps/one
+    /two       => apps/two
+
+  Care is taken to match the apps with the longest URLs first, because
+  they are the most specific.
+
+  If you need more fine-grained control over the server's configuration,
+  you can use a JavaScript module that exports an array of { path, root, options }
+  objects where:
+
+    - path is the URL pattern, i.e. /the/url*
     - root is the root directory of the app on disk
-    - options are any options to express.static
+    - options are express.static options
 
-  Otherwise just point history-server at a root directory that
-  will be served at the / URL.
-`)
+  Then use that file with "history-server -c config.js".
+  `)
+}
+
+const printConfig = (config) => {
+  const longestPath = config.reduce((memo, { path }) => {
+    return memo.length > path.length ? memo : path
+  }, '')
+
+  config.forEach(({ path: pattern, root }) => {
+    const trailingSpaces = longestPath.length - pattern.length
+    const trailingSpace = new Array(trailingSpaces + 1).join(' ')
+    console.log('  %s%s => %s', pattern, trailingSpace, path.relative(process.cwd(), root))
+  })
+}
+
+if (hasFlag('-h') || hasFlag('--help')) {
+  printUsage()
+  process.exit(0)
+}
+
+const startServer = (config) => {
+  const server = createServer(config)
+  const port = getArg('-p') || getArg('--port') || 8080
+
+  server.listen(port, () => {
+    console.log('history-server listening on port %s; Ctrl+C to stop', port)
+  })
+}
+
+const appsDir = getArg('-a') || getArg('--apps')
+const configFile = getArg('-c') || getArg('--config')
+
+if (appsDir) {
+  const appsConfig = createAppsConfig(appsDir)
+
+  console.log('history-server serving all apps in %s', path.resolve(appsDir))
+
+  printConfig(appsConfig)
+  startServer(appsConfig)
+} else if (configFile) {
+  console.log('history-server using config in %s', configFile)
+
+  let config
+  try {
+    config = require(path.resolve(configFile))
+  } catch (error) {
+    console.error('Invalid config file: %s', configFile)
+    process.exit(1)
   }
 
-  const startServer = (config) => {
-    const server = createServer(config)
-    const port = getArg('-p') || getArg('--port') || 8080
+  printConfig(config)
+  startServer(config)
+} else {
+  const root = argv[2]
 
-    server.listen(port, () => {
-      console.log('history server listening on port %s; Ctrl+C to stop', port)
-    })
-  }
-
-  if (hasFlag('-h') || hasFlag('--help')) {
-    showUsage()
-    process.exit(0)
-  }
-
-  const configFile = getArg('-c') || getArg('--config')
-
-  if (configFile) {
-    console.log('history server using config in %s', configFile)
-
-    let config
-    try {
-      config = require(path.resolve(configFile))
-    } catch (error) {
-      console.error('Invalid config file: %s', configFile)
-      process.exit(1)
-    }
-
-    startServer(config)
+  if (root) {
+    startServer([ { path: '/', root } ])
   } else {
-    const root = argv[2]
-
-    if (root) {
-      startServer([ { path: '/', root } ])
-    } else {
-      showUsage()
-      process.exit(1)
-    }
+    printUsage()
+    process.exit(1)
   }
-})()
+}
--- modules/index.js
+++ modules/index.js
@@ -1,22 +1,43 @@
-const resolvePath = require('path').resolve
+const path = require('path')
+const glob = require('glob')
 const express = require('express')
 
 const createServer = (config = []) => {
   const app = express()
 
-  config.forEach(({ path, root, options = {} }) => {
+  config.forEach(({ path: pattern, root, options = {} }) => {
     // Try to send a static file.
-    app.use(path, express.static(root, options))
+    app.use(pattern, express.static(root, options))
 
     // Fall back to the HTML file.
-    app.use(path, (req, res) => {
-      res.sendFile(resolvePath(root, options.index || 'index.html'))
+    app.use(pattern, (req, res) => {
+      res.sendFile(path.resolve(root, options.index || 'index.html'))
     })
   })
 
   return app
 }
 
+const byLengthDescending = (a, b) =>
+  b.length - a.length
+
+const createAppsConfig = (appsDir) => {
+  const files = glob.sync(path.join(appsDir, '**', 'index.html'))
+  const paths = files.map(file => path.dirname(file.replace(appsDir, '')))
+
+  paths.sort(byLengthDescending)
+
+  return paths.map(pattern => ({
+    path: pattern,
+    root: path.join(appsDir, pattern)
+  }))
+}
+
+const createAppsServer = (appsDir) =>
+  createServer(createAppsConfig(appsDir))
+
 module.exports = {
-  createServer
+  createServer,
+  createAppsConfig,
+  createAppsServer
 }
