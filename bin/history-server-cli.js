d4d125dbe389e2929d1218f1ad2062d63295556f
MICHAEL JACKSON <mjijackson@gmail.com>
2017-04-20 15:08:26 -0700

    Update CLI usage instructions

--- bin/history-server-cli.js
+++ bin/history-server-cli.js
@@ -14,68 +14,21 @@ const getArg = (flag, defaultValue) => {
 
 const printUsage = () => {
   console.log(`
-  ## Usage
+  Usage:
 
-    The easiest way to use history-server is to point the binary at an "app"
-    directory that contains your index.html file.
+    history-server app
+    history-server -a apps
+    history-server -c config.js
 
-      $ history-server app
+  You may use the following flags:
 
-    Alternatively, you may serve many apps from the same domain using a
-    directory that contains many apps or a config file (see Configuration below).
+    -h, --help      Show this help message
+    -a, --apps      The path to a directory that contains many apps
+    -c, --config    The path to a module that exports the server config
+    -p, --port      The port to bind to, defaults to 8080
 
-      $ history-server -a apps
-      $ history-server -c config.js
-
-    You can use the following flags:
-
-      -h, --help      Show this help message
-      -a, --apps      The path to a directory that contains many apps
-      -c, --config    The path to a module that exports the server config
-      -p, --port      The port to bind to, defaults to 8080
-
-  ## Configuration
-
-  To serve a single app, just point history-server at a root directory
-  that contains an index.html file to serve at the / URL, using e.g.
-  "history-server app".
-
-  To serve many apps, you'll need a way to tell history-server which apps
-  should be served at which URLs. The simplest way to do this is to just
-  use the file system to layout your apps like you want your URLs to look.
-
-  For example, consider the following directory tree:
-
-    apps/
-    ├── one
-    │   ├── index.html
-    │   └── index.js
-    └── two
-        ├── index.html
-        ├── index.js
-        └── three
-            ├── index.html
-            └── index.js
-
-  You can use "history-server -a apps" serve all 3 of these apps at the
-  following URLs, in matching order:
-
-    /two/three => apps/two/three
-    /one       => apps/one
-    /two       => apps/two
-
-  Care is taken to match the apps with the longest URLs first, because
-  they are the most specific.
-
-  If you need more fine-grained control over the server's configuration,
-  you can use a JavaScript module that exports an array of { path, root, options }
-  objects where:
-
-    - path is the URL pattern, i.e. /the/url*
-    - root is the root directory of the app on disk
-    - options are express.static options
-
-  Then use that file with "history-server -c config.js".
+  See https://github.com/mjackson/history-server#configuration for more
+  information about configuration.
   `)
 }
 
