4c4b9838f78dbd907ab50f2a7b86b994261f99b1
MICHAEL JACKSON <mj@mjackson.me>
2018-02-08 15:13:11 -0800

    Prettify

--- history-server.js
+++ history-server.js
@@ -1,18 +1,19 @@
 #!/usr/bin/env node
-const path = require('path')
-const { createServer, createAppsConfig } = require('./index')
+const path = require("path");
+const { createServer, createAppsConfig } = require("./index");
 
-const argv = process.argv
+const argv = process.argv;
 
-const hasFlag = (flag) =>
-  argv.indexOf(flag) !== -1
-
-const getArg = (flag, defaultValue) => {
-  const index = argv.indexOf(flag)
-  return index === -1 ? defaultValue : argv[index + 1]
+function hasFlag(flag) {
+  return argv.indexOf(flag) !== -1;
 }
 
-const printUsage = () => {
+function getArg(flag, defaultValue) {
+  const index = argv.indexOf(flag);
+  return index === -1 ? defaultValue : argv[index + 1];
+}
+
+function printUsage() {
   console.log(`
   Usage:
 
@@ -29,71 +30,71 @@ const printUsage = () => {
 
   See https://github.com/mjackson/history-server#configuration for more
   information about configuration.
-  `)
+  `);
 }
 
-const printConfig = (config) => {
+function printConfig(config) {
   const longestPath = config.reduce((memo, { path }) => {
-    return memo.length > path.length ? memo : path
-  }, '')
+    return memo.length > path.length ? memo : path;
+  }, "");
 
   config.forEach(({ path: pattern, proxy, root }) => {
-    const trailingSpaces = longestPath.length - pattern.length
-    const trailingSpace = new Array(trailingSpaces + 1).join(' ')
+    const trailingSpaces = longestPath.length - pattern.length;
+    const trailingSpace = new Array(trailingSpaces + 1).join(" ");
 
     console.log(
-      '  %s%s => %s',
+      "  %s%s => %s",
       pattern,
       trailingSpace,
       (proxy && proxy.target) || proxy || path.relative(process.cwd(), root)
-    )
-  })
+    );
+  });
 }
 
-if (hasFlag('-h') || hasFlag('--help')) {
-  printUsage()
-  process.exit(0)
+if (hasFlag("-h") || hasFlag("--help")) {
+  printUsage();
+  process.exit(0);
 }
 
-const startServer = (config) => {
-  const server = createServer(config)
-  const port = getArg('-p') || getArg('--port') || 8080
+function startServer(config) {
+  const server = createServer(config);
+  const port = getArg("-p") || getArg("--port") || 8080;
 
   server.listen(port, () => {
-    console.log('history-server listening on port %s; Ctrl+C to stop', port)
-  })
+    console.log("history-server listening on port %s; Ctrl+C to stop", port);
+  });
 }
 
-const appsDir = getArg('-a') || getArg('--apps')
-const configFile = getArg('-c') || getArg('--config')
+const appsDir = getArg("-a") || getArg("--apps");
+const configFile = getArg("-c") || getArg("--config");
 
 if (appsDir) {
-  const appsConfig = createAppsConfig(appsDir)
+  const appsConfig = createAppsConfig(appsDir);
 
-  console.log('history-server serving all apps in %s', path.resolve(appsDir))
+  console.log("history-server serving all apps in %s", path.resolve(appsDir));
 
-  printConfig(appsConfig)
-  startServer(appsConfig)
+  printConfig(appsConfig);
+  startServer(appsConfig);
 } else if (configFile) {
-  console.log('history-server using config in %s', configFile)
+  console.log("history-server using config in %s", configFile);
 
-  let config
+  let config;
   try {
-    config = require(path.resolve(configFile))
+    config = require(path.resolve(configFile));
   } catch (error) {
-    console.error('Invalid config file: %s', configFile)
-    process.exit(1)
+    console.error("Invalid config file: %s", configFile);
+    process.exit(1);
   }
 
-  printConfig(config)
-  startServer(config)
+  printConfig(config);
+  startServer(config);
 } else {
-  const root = argv[2]
+  const root = argv[2];
 
   if (root) {
-    startServer([ { path: '/', root } ])
+    startServer([{ path: "/", root }]);
   } else {
-    printUsage()
-    process.exit(1)
+    printUsage();
+    process.exit(1);
   }
 }
--- index.js
+++ index.js
@@ -1,54 +1,56 @@
-const path = require('path')
-const glob = require('glob')
-const express = require('express')
-const httpProxy = require('http-proxy')
+const path = require("path");
+const glob = require("glob");
+const express = require("express");
+const httpProxy = require("http-proxy");
 
-const createServer = (config = []) => {
-  const app = express()
-  const pxy = httpProxy.createProxyServer()
+function createServer(config = []) {
+  const app = express();
+  const pxy = httpProxy.createProxyServer();
 
   config.forEach(({ path: pattern, root, proxy, options = {} }) => {
     if (proxy) {
-      const options = typeof proxy === 'string' ? { target: proxy } : proxy
+      const options = typeof proxy === "string" ? { target: proxy } : proxy;
 
       // Proxy the request to a different host.
       app.use(pattern, (req, res) => {
-        pxy.web(req, res, options)
-      })
+        pxy.web(req, res, options);
+      });
     } else {
       // Try to send a static file.
-      app.use(pattern, express.static(root, options))
+      app.use(pattern, express.static(root, options));
 
       // Fall back to the HTML file.
       app.use(pattern, (req, res) => {
-        res.sendFile(path.resolve(root, options.index || 'index.html'))
-      })
+        res.sendFile(path.resolve(root, options.index || "index.html"));
+      });
     }
-  })
+  });
 
-  return app
+  return app;
 }
 
-const byLengthDescending = (a, b) =>
-  b.length - a.length
+function byLengthDescending(a, b) {
+  return b.length - a.length;
+}
 
-const createAppsConfig = (appsDir) => {
-  const files = glob.sync(path.join(appsDir, '**', 'index.html'))
-  const paths = files.map(file => path.dirname(file.replace(appsDir, '')))
+function createAppsConfig(dir) {
+  const files = glob.sync(path.join(dir, "**", "index.html"));
+  const paths = files.map(file => path.dirname(file.replace(dir, "")));
 
-  paths.sort(byLengthDescending)
+  paths.sort(byLengthDescending);
 
   return paths.map(pattern => ({
     path: pattern,
-    root: path.join(appsDir, pattern)
-  }))
+    root: path.join(dir, pattern)
+  }));
 }
 
-const createAppsServer = (appsDir) =>
-  createServer(createAppsConfig(appsDir))
+function createAppsServer(dir) {
+  return createServer(createAppsConfig(dir));
+}
 
 module.exports = {
   createServer,
   createAppsConfig,
   createAppsServer
-}
+};
